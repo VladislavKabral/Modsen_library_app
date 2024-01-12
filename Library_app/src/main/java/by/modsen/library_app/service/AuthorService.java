c204b5731b510f3d40938608b16cad719c614757
VladislavKabral <mr.kabral@mail.ru>
2024-01-12 18:42:04 +0300

    Delete unnecessary methods

--- Library_app/src/main/java/by/modsen/library_app/service/AuthorService.java
+++ Library_app/src/main/java/by/modsen/library_app/service/AuthorService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,22 +20,6 @@ public class AuthorService {
         this.authorRepository = authorRepository;
     }
 
-    public List<Author> findAll() throws EntityNotFoundException {
-        List<Author> authors = authorRepository.findAll();
-
-        if (authors.isEmpty()) {
-            throw new EntityNotFoundException("There aren't any authors in the library");
-        }
-
-        return authors;
-    }
-
-    public Author findById(int id) throws EntityNotFoundException {
-        Optional<Author> author = authorRepository.findById(id);
-
-        return author.orElseThrow(EntityNotFoundException.entityNotFoundException("Author with id: " + id + " not found"));
-    }
-
     public Author findByLastnameAndFirstname(String lastname, String firstname) throws EntityNotFoundException {
         Optional<Author> author = authorRepository.findByLastnameAndFirstname(lastname, firstname);
 
@@ -47,22 +30,5 @@ public class AuthorService {
     @Transactional
     public void save(Author author) {
         authorRepository.save(author);
-    }
-
-    @Transactional
-    public void updateById(int id, Author author) throws EntityNotFoundException {
-        Author authorFromDB = findById(id);
-
-        authorFromDB.setLastname(author.getLastname());
-        authorFromDB.setFirstname(author.getFirstname());
-
-        authorRepository.save(authorFromDB);
-    }
-
-    @Transactional
-    public void deleteById(int id) throws EntityNotFoundException {
-        Author author = findById(id);
-
-        authorRepository.deleteById(author.getId());
     }
 }
--- Library_app/src/main/java/by/modsen/library_app/service/GenreService.java
+++ Library_app/src/main/java/by/modsen/library_app/service/GenreService.java
@@ -6,7 +6,6 @@ import by.modsen.library_app.util.exception.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,47 +18,9 @@ public class GenreService {
         this.genreRepository = genreRepository;
     }
 
-    public List<Genre> findAll() throws EntityNotFoundException {
-        List<Genre> genres = genreRepository.findAll();
-
-        if (genres.isEmpty()) {
-            throw new EntityNotFoundException("There aren't any genres in the library");
-        }
-
-        return genres;
-    }
-
-    public Genre findById(int id) throws EntityNotFoundException {
-        Optional<Genre> genre = genreRepository.findById(id);
-
-        return genre.orElseThrow(EntityNotFoundException.entityNotFoundException("Genre with id: " + id + " not found"));
-    }
-
     public Genre findByName(String name) throws EntityNotFoundException {
         Optional<Genre> genre = genreRepository.findByName(name);
 
         return genre.orElseThrow(EntityNotFoundException.entityNotFoundException("Genre with name '" + name + "' not found"));
-    }
-
-    @Transactional
-    public void save(Genre genre) {
-        genreRepository.save(genre);
-    }
-
-    @Transactional
-    public void updateById(int id, Genre genre) throws EntityNotFoundException {
-        Genre genreFromDB = findById(id);
-
-        genreFromDB.setName(genre.getName());
-        genreFromDB.setDescription(genre.getDescription());
-
-        genreRepository.save(genreFromDB);
-    }
-
-    @Transactional
-    public void deleteById(int id) throws EntityNotFoundException {
-        Genre genre = findById(id);
-
-        genreRepository.deleteById(genre.getId());
     }
 }
